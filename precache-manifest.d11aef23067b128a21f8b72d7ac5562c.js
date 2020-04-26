824fe10bcbe4c8a36e0b6254f5dbe39e88cafaf8
Ali Bayatpour <aliglbayatpour@gmail.com>
2020-04-26 12:29:39 +0300

    Updates

---
+++ precache-manifest.d11aef23067b128a21f8b72d7ac5562c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4655556e4b265b7d7810fbff6579c7f",
+    "url": "/agency/index.html"
+  },
+  {
+    "revision": "bcd3c4bc9a19230a1559",
+    "url": "/agency/static/css/main.68770233.chunk.css"
+  },
+  {
+    "revision": "c3318e92b59afeb08d4a",
+    "url": "/agency/static/js/2.211d7cba.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/agency/static/js/2.211d7cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd3c4bc9a19230a1559",
+    "url": "/agency/static/js/main.9ae7a67f.chunk.js"
+  },
+  {
+    "revision": "50b54b15b4f091932b91",
+    "url": "/agency/static/js/runtime-main.f267fad2.js"
+  },
+  {
+    "revision": "6860fd03e99aa12a2f8bb90d54612b8e",
+    "url": "/agency/static/media/arrowUp.6860fd03.svg"
+  },
+  {
+    "revision": "28747b8d5613ce3d304216375dae972d",
+    "url": "/agency/static/media/leftArrow.28747b8d.svg"
+  },
+  {
+    "revision": "07a07b3e0afc616f9f8e37ade1ebecca",
+    "url": "/agency/static/media/logo.07a07b3e.svg"
+  },
+  {
+    "revision": "b0db3c7d970b79e678c6040115b53b06",
+    "url": "/agency/static/media/rightArrow.b0db3c7d.svg"
+  }
+]);
